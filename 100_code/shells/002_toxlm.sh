57cf000206b82fa9516dd570be64eab39977bb46
ALI <ali.limon.m@gmail.com>
2016-07-05 13:04:17 +0200

    remove 002_xml.sh

--- 100_code/shells/002_toxlm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find ~001_raw_data/pdf/ -name "*.pdf" | while read file;
-do
-curl --data-binary @"$file" -H "Content-Type: application/pdf" -L "http://pdfx.cs.man.ac.uk" > "${file}x.xml";
-done
