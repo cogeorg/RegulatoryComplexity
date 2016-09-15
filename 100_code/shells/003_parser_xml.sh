0388bd2f00f57ca7e7d1155e0b2f06fe0287fdac
Co-Pierre Georg <cogeorg@gmail.com>
2016-09-15 16:06:29 +0200

    Added shell scripts for last refactorization

--- 100_code/shells/003_parser_xml.sh
+++ 100_code/shells/003_parser_xml.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Set working directory 
-cd /Users/alilimon/Documents/Research/ &&
-    
+#cd /Users/alilimon/Documents/Research/ &&
+cd /Users/co/git/ 
+
 python ./RegulatoryComplexity/100_code/python/026_xml_parser/xml_parser.py -i ./RegulatoryComplexity/001_raw_data/html/dodd_frank.html -o  ./RegulatoryComplexity/010_cleaned_data/
--- 100_code/shells/005_create_visuals.sh
+++ 100_code/shells/005_create_visuals.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-cd /Users/alilimon/Documents/Research/ &&
+#cd /Users/alilimon/Documents/Research/ &&
+cd /Users/co/git/
 
 python ./RegulatoryComplexity/100_code/python/030_create_visuals/create_visuals.py -i  ./RegulatoryComplexity/010_cleaned_data/dodd_frank.xml -w ./RegulatoryComplexity/020_auxiliary_data/Sections/Protected_list/ -o  ./RegulatoryComplexity/050_results/DoddFrank/Visuals/DF_Templates_protected/
