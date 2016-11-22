8f155b7426db80e5943b089fa36d5feb48bf4eb4
sabinebertram <sabine.bertram@mailbox.org>
2016-11-22 18:17:50 +0100

    updated look of sentence part, started on possibility to create graphs

--- 050_results/DoddFrank/Visuals/Visualizer_Versions/C1_combined/apps/sentences/static/js/main.js
+++ 050_results/DoddFrank/Visuals/Visualizer_Versions/C1_combined/apps/sentences/static/js/main.js
@@ -1,4 +1,40 @@
-
+// Function to create the graphs (tables)
+function graphs(){
+    var html = document.getElementById('result').innerHTML
+    // extract title name
+    var title = /(TITLE\s.*?)<div/g.exec(html)[1]
+    $("#graph").html('<div class = "ex1">' + title + '</div>')
+    // extract sections and corresponding paragraphs
+    var pattern = /(SEC\.\s[0-9].+?\.\s[A-Z].*?\.) | (class="ex5")/g
+    pattern.lastIndex = 0;
+    id = 0
+    while (section = pattern.exec(html)){
+        // paste section name
+        if (String(section[1]).startsWith('SEC.') == true){
+            $("#graph").append('<div class = "ex4">' + section[1] + '</div>')
+        }
+        // generate placeholder for paragraphs
+        else {
+            $("#graph").append('<div class = "table" id="'+ id + '"></div>')
+            id += 1
+        }
+    }
+    // extract paragraphs
+    var paragraphs = document.getElementsByClassName('ex5')
+    for(var i = 0; i < paragraphs.length; i++) {
+        // extract HTML
+        var parHtml = paragraphs[i].innerHTML
+        // extract spans
+        var regex = /<sp.*?<\/span>/g
+        if (regex.test(parHtml) == true){
+            var spans = parHtml.match(regex)
+            for(var j = 0; j < spans.length; j++) {
+                // create table
+                $("#" + String(i)).append(spans[j])
+            }
+        }
+    }
+}
 
 (function () {
     /* First p tag*/
@@ -6,7 +42,7 @@
 
     /* Load different titles*/
     /* ******************** */
-    document.getElementById("mySidenav").style.width = "150px";
+    /*document.getElementById("mySidenav").style.width = "120px";*/
 
     var auxiliar = "";
      $("#mySidenav").click(function(){
@@ -27,6 +63,7 @@
             $.get(line, function(res) {
             // load html title
             $("#result").html(res);
+            graphs()
             });
         });
     }
