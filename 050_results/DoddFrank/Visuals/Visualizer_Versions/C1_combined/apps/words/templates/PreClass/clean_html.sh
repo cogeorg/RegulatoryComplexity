bcb78a00325112a64e7f753785b72d2c4a40fd11
Co-Pierre Georg <cogeorg@gmail.com>
2017-10-17 23:53:13 +0200

    added cleaning file

---
+++ 050_results/DoddFrank/Visuals/Visualizer_Versions/C1_combined/apps/words/templates/PreClass/clean_html.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sed s/' <span class="EconomicOperands"> class <\/span> = "ex1">'/'>'/ < title_492.html >foo
+sed s/' <span class="EconomicOperands"> class <\/span> = "ex2">'/'>'/ < title_492.html >foo
+sed s/' <span class="EconomicOperands"> class <\/span> = "ex3">'/'>'/ < title_492.html >foo
+sed s/' <span class="EconomicOperands"> class <\/span> = "ex4">'/'>'/ < title_492.html >foo
+sed s/' <span class="EconomicOperands"> class <\/span> = "ex5">'/'>'/ < title_492.html >foo
+sed s/' <span class="EconomicOperands"> class <\/span> = "ex6">'/'>'/ < title_492.html >foo
+sed s/' <span class="EconomicOperands"> class <\/span> = "ex7">'/'>'/ < title_492.html >foo
+sed s/' <span class="EconomicOperands"> class <\/span> = "ex8">'/'>'/ < title_492.html >foo
+sed s/' <span class="EconomicOperands"> class <\/span> = "ex9">'/'>'/ < title_492.html >foo
+sed s/' <span class="EconomicOperands"> class <\/span> = "ex10">'/'>'/ < title_492.html >foo
