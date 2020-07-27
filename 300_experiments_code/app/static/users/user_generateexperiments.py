de10bb88f14f7c9deaa739323bf89c4b39bc68d0
Co-Pierre Georg <cogeorg@gmail.com>
2020-07-27 19:56:46 +0200

    worked on user experiment list
    randomization works, everything should work in the app now.

--- 300_experiments_code/app/static/users/user_generateexperiments.py
+++ 300_experiments_code/app/static/users/user_generateexperiments.py
@@ -4,9 +4,9 @@ import numpy as np
 # users = [[1, 'john.doe@hec.edu'], [2, 'anna@hec.edu'], [3, 'clara@hec.edu'], [4, 'luc@gmail.com'], [5, 'sara@gmail.com'], [6, 'lucie@gmail.com'], [7, 'paul@gmail.com'], [8, 'juuli@gmail.com'], [9, 'j@ghj.com'], [10, 'benjamin@gmail.com']]
 
 i=0
-while i<100:
+while i<1000:
     with open("user_%d_experiments.csv"%i, "w") as file:
-        user_experiments = random.sample(np.arange(0,43,1).tolist(), 10)
+        user_experiments = random.sample(np.arange(0,39,1).tolist(), 10)
         for a in user_experiments :
             file.write(str(a) + "\n")
     i += 1
