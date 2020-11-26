11503d33e977921a7e10a7e579c326f29f87e501
Emile Pretorius <emile.dpretorius@gmail.com>
2020-11-26 14:28:43 +0200

    title change

--- 300_experiments_code/app/forms.py
+++ 300_experiments_code/app/forms.py
@@ -33,5 +33,5 @@ class RulesForm(FlaskForm):
     submit = SubmitField("Continue")
 
 class SubmissionForm(FlaskForm):
-    answer = FloatField("Please enter the bank's total risk weighted assets for this regulation", validators = [DataRequired()])
+    answer = FloatField("Enter the bank's total risk weighted assets for this regulation:", validators = [DataRequired()])
     submit = SubmitField("Save and continue")
