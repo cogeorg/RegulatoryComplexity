510c5083364b1c564745b6858140764e945ab358
Co-Pierre Georg <cogeorg@protonmail.com>
2020-12-01 19:00:48 +0200

    Added Pillar1+2 Tax

--- 710_tax/200_analysis/01_do_count.sh
+++ 710_tax/200_analysis/01_do_count.sh
@@ -3,6 +3,8 @@
 # ./05_prepare_glossary.py ../020_word_lists/RegulatoryOperators_Tax-raw.txt ../020_word_lists/RegulatoryOperators_Tax-clean.txt
 
 # Then do analysis
+time ./10_do_analysis.py ../010_raw_documents/Pillar1.txt Pillar1.csv
+time ./10_do_analysis.py ../010_raw_documents/Pillar2.txt Pillar2.csv
 time ./10_do_analysis.py ../010_raw_documents/OECD.txt OECD.csv
 time ./10_do_analysis.py ../010_raw_documents/UN.txt UN.csv
 
