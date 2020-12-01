7b9bd4e2371dff33163397c5413b08f062f4f53f
Emile Pretorius <emile.dpretorius@gmail.com>
2020-12-01 12:17:12 +0200

    removed table files and submission2 file

--- 300_experiments_code/app/tables.py
+++
@@ -1,6 +0,0 @@
-from flask_table import Table, Col, LinkCol
-
-class Results(Table):
-    id = Col('Id', show=True)
-    correctanswer = Col('Correct Answer', show=True)
-    Col('Your Answer', show=True)
